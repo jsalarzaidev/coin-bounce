b52f7ae39807f224b73de503e98221f3ddbbcdb8
jsalarzaidev <jahanzaibsalarzai.dev@outlook.com>
2023-09-16 17:32:15 +0500

    email validation

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,5 +1,56 @@
+const Joi = require("joi");
+const User = require("../models/user");
 const authController = {
-  async register() {},
+  async register(req, res, next) {
+    const passwordPattern = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,25}$/;
+    // 1. validate user input
+    const userRegisterSchema = Joi.object({
+      username: Joi.string().min(5).max(30).required(),
+      name: Joi.string().max(30).required(),
+      email: Joi.string().email().required(),
+      password: Joi.string().pattern(passwordPattern).required(),
+      confirmPassword: Joi.ref("password"),
+    });
+
+    const { error } = userRegisterSchema.validate(req.body);
+
+    // 2. if error in validation -> return error via middleware
+    if (error) {
+      return next(error);
+    }
+    // 3. if email or username is already registered -> return an error
+    const { username, name, email, password } = req.body;
+
+    try {
+      const emailInUse = await User.exists({ email });
+
+      const usernameInUse = await User.exists({ username });
+
+      if (emailInUse) {
+        const error = {
+          status: 409,
+          message: "Email is already in use, use another email!",
+        };
+
+        return next(error);
+      }
+
+      if (usernameInUse) {
+        const error = {
+          status: 409,
+          message: "username not available, choose another username!",
+        };
+
+        return next(error);
+      }
+    } catch (error) {
+      return next(error);
+    }
+
+    // 4. password hash if no error returned
+    // 5. store user data in db
+    // 6. respond send
+  },
   async login() {},
 };
 
---
+++ backend/middlewares/errorHandler.js
@@ -0,0 +1,45 @@
+const { validationError } = require("joi");
+const errorHandler = (error, req, res, next) => {
+  // default error
+  let status = 500;
+  let data = {
+    message: "Internal Server Error",
+  };
+  /*
+  if (error instanceof validationError) {
+    status = error.status;
+    data.message = error.message;
+    return res.status(status).json(data);
+  }
+
+  if (error.status) {
+    status = error.status;
+  }
+
+  if (error.message) {
+    data.message = error.message;
+  }
+
+
+  return res.status(status).json(data);
+*/
+
+  if (error instanceof validationError) {
+    status = 401;
+    data.message = error.message;
+
+    return res.status(status).json(data);
+  }
+
+  if (error.status) {
+    status = error.status;
+  }
+
+  if (error.message) {
+    data.message = error.message;
+  }
+
+  return res.status(status).json(data);
+};
+
+module.exports = errorHandler;
